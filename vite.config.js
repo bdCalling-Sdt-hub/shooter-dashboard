fe20f0ba11a519aee3505c9bce9f1b6496bd996a
Sahinur Islam <infosahinur@gmail.com>
2024-09-23 18:12:14 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "192.168.10.149",
+    host: "0.0.0.0",
     port: "8000",
   },
 });
